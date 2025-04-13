e942a4b0c867d135822aa15e9a3e6ed9efe900f4
Luis Manuel <luis_duarte@ucne.edu.do>
2025-04-12 21:22:48 -0600

    migracion

---
+++ BusinessGenesis/Data/GenesisContexFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using BusinessGenesis.Data;  // Asegúrate de que el espacio de nombres sea correcto
+
+namespace BusinessGenesis.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<GenesisContex>
+    {
+        public GenesisContex CreateDbContext(string[] args)
+        {
+            // Leer la configuración del appsettings.json
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Obtener la cadena de conexión
+            var connectionString = configuration.GetConnectionString("ConStr");
+
+            // Configurar las opciones del DbContext
+            var optionsBuilder = new DbContextOptionsBuilder<GenesisContex>();
+            optionsBuilder.UseSqlite(connectionString);  // Cambiar a UseSqlite
+
+            return new GenesisContex(optionsBuilder.Options);
+        }
+    }
+}
