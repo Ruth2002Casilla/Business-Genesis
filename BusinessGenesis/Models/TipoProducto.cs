a5140ef3642bc12be1d9264c5ab5cb05d2182a2e
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2025-04-14 21:14:12 -0400

    Agrega el Modelo TipoProductos

---
+++ BusinessGenesis/Models/TipoProducto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusinessGenesis.Models
+{
+    public class TipoProducto
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("IdNegocio")]
+        public int idNegocio {  get; set; }
+
+        public string? nombre { get; set; }
+
+    }
+}
